9f86ef94f9ab0a2255d27e83d451778888a2f442
shifenteng <shifenteng@matech.cn>
2021-01-24 20:20:40 +0800

    路由

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service'
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +11,23 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero | any;
+  // @Input() hero: Hero | any;
 
-  constructor() { }
+  hero: Hero | any;
+
+  constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id).subscribe((hero: any) => this.hero = hero)
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
   ngOnInit(): void {
+    this.getHero()
   }
 
 }
