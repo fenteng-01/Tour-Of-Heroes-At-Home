4256040f30d2dec514965974a3f2129a6415d591
shifenteng <shifenteng@matech.cn>
2021-01-29 15:10:21 +0800

    hero-search

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import { Hero } from '../hero';
 import { ActivatedRoute } from '@angular/router';
-import { HeroService } from '../hero.service'
 import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service'
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: ['./hero-detail.component.css'],
 })
-export class HeroDetailComponent implements OnInit {
 
-  // @Input() hero: Hero | any;
+export class HeroDetailComponent implements OnInit {
 
   hero: Hero | any;
 
   constructor(private route: ActivatedRoute, private heroService: HeroService, private location: Location) { }
+
+  ngOnInit(): void {
+    this.getHero()
+  }
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -28,10 +32,6 @@ export class HeroDetailComponent implements OnInit {
 
   save(): void {
     this.heroService.updateHero(this.hero).subscribe(() => this.goBack())
-  }
-
-  ngOnInit(): void {
-    this.getHero()
   }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -10,13 +9,10 @@ import { MessageService } from '../message.service';
 export class HeroesComponent implements OnInit {
   heroes: Hero | any;
 
-  constructor(private heroService: HeroService, private messageService: MessageService) { }
+  constructor(private heroService: HeroService) { }
 
-  selectedHero: Hero | any;
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  ngOnInit(): void {
+    this.getHeroes();
   }
 
   getHeroes(): void {
@@ -29,8 +25,9 @@ export class HeroesComponent implements OnInit {
     this.heroService.addHero({ name } as Hero).subscribe(hero => { this.heroes.push(hero) })
   }
 
-  ngOnInit(): void {
-    this.getHeroes();
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h: Hero) => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 
 }
